f1bba32c33c2acf6d4c6aca4255883d2a9f4f52c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-01-25 13:47:15 +0900

    Update Find-Digits.cpp

--- Find-Digits.cpp
+++ Find-Digits.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
 int findDigits(int n)
  {
     int number = n;
